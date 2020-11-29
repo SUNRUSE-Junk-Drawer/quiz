fe1ff8012a841f5babb0d18862a07c8cf325a53c
jameswilddev <jameswilddev@outlook.com>
2020-11-29 23:48:39 +0000

    Add dummy prompt server.

---
+++ host/aws/server/index.ts
@@ -0,0 +1,6 @@
+import { hostApiGatewayLambdaWebsocketEventHandler } from "@endless-trash/api-gateway-lambda-websocket-host";
+import { eventHandler } from "../../server";
+
+export const handler = hostApiGatewayLambdaWebsocketEventHandler<
+  Record<string, unknown>
+>({}, {}, eventHandler);
---
+++ host/server/index.ts
@@ -0,0 +1,77 @@
+import { EventHandler } from "@endless-trash/event-handler";
+import { Prompt } from "@endless-trash/prompt";
+import {
+  WebsocketHostInput,
+  WebsocketHostOutput,
+} from "@endless-trash/websocket-host";
+
+export const eventHandler: EventHandler<
+  WebsocketHostInput,
+  WebsocketHostOutput
+> = async (event) => {
+  const prompt: Prompt = {
+    formGroups: [
+      {
+        name: `Test Form Group`,
+        forms: [
+          {
+            name: `Test Form`,
+            submitButtonLabel: `Test Submit Button Label`,
+            fields: [
+              {
+                type: `title`,
+                name: `Test Title Field`,
+                content: `Test Title Content`,
+              },
+              {
+                type: `subtitle`,
+                name: `Test Subtitle Field`,
+                content: `Test Subtitle Content`,
+              },
+              {
+                type: `paragraph`,
+                name: `Test Paragraph Field`,
+                content: `Test Paragraph Content`,
+              },
+              {
+                type: `string`,
+                name: `Test String Field`,
+                label: `Test String Label`,
+                minimumLength: 5,
+                maximumLength: 25,
+                value: ``,
+              },
+              {
+                type: `integer`,
+                name: `Test Integer Field`,
+                label: `Test Integer Label`,
+                minimum: [5, `inclusive`],
+                maximum: [20, `inclusive`],
+                value: null,
+                required: true,
+              },
+              {
+                type: `float`,
+                name: `Test Integer Field`,
+                label: `Test Integer Label`,
+                minimum: [5, `inclusive`],
+                maximum: [20, `inclusive`],
+                value: null,
+                required: true,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  return {
+    messages: [
+      {
+        sessionId: event.sessionId,
+        body: JSON.stringify(prompt),
+      },
+    ],
+  };
+};
